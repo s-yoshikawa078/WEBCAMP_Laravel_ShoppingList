3adfda2100d50b125414315ba3548abb2a1cb40d
yoshikawa <s-yoshikawa078@users.noreply.github.com>
2025-12-08 03:16:31 +0000

    課題再提出 ２ 回目

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,22 +8,27 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
+    // 会員登録画面
+    public function index()
+    {
+        return view('user.register');
+    }
+
+    // 会員登録処理
     public function register(Request $request)
     {
-        // バリデーション追加
         $validatedData = $request->validate([
             'name' => 'required|string|max:128',
             'email' => 'required|string|email|max:254|unique:users,email',
             'password' => 'required|string|confirmed|max:72',
         ]);
 
-        // 登録処理
         User::create([
             'name' => $validatedData['name'],
             'email' => $validatedData['email'],
             'password' => Hash::make($validatedData['password']),
         ]);
 
-        return redirect()->route('front.user.register')->with('success', 'ユーザ登録が完了しました');
+        return redirect()->route('front.user.register')->with('success', 'ユーザ登録が完了しました！！');
     }
 }
